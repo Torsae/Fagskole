e3e225b6c788cbfef7a03581f1f21fea6ef67dc2
Torsae <157711034+Torsae@users.noreply.github.com>
2025-02-11 14:59:51 +0100

    assignments

--- Inside/1.5/1-Loops/A/view.js
+++
@@ -1,8 +0,0 @@
-function updateView() {
-    document.getElementById("app").innerHTML = /*HTML*/`
-    <input type='text' id='textInput'>
-    <input type='number' id='numberInput'>
-    <button onclick='multiplyText(textInput.value, numberInput.valueAsNumber)'>Multiply Text</button>
-    <div id="errorMessage"></div>
-    `;
-}
